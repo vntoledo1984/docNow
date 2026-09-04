cffcfa5a2d4316e60a2ad0fb48bcf567ac407bf3
vntoledo1984 <vntoledo1984@gmail.com>
2026-09-04 15:28:39 -0300

    TESTE

--- docNow/src/main/java/docNow/Teste.java
+++ docNow/src/main/java/docNow/Teste.java
@@ -3,6 +3,6 @@ package docNow;
 public class Teste {
 	
 	public static void main(String[] args) {
-		System.out.println("AEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+		System.out.println("AEEEEEEEEEEEEEEEEEEEEEEE");
 	}
 }
